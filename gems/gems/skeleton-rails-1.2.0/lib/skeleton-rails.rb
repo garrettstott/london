6cd5769910540dc624e6f2e69ac778febcc76a0c
Garrett Stott <garrettstott@gmail.com>
2019-10-05 17:42:47 -0600

    removed gems from project

--- gems/gems/skeleton-rails-1.2.0/lib/skeleton-rails.rb
+++
@@ -1,6 +0,0 @@
-require "skeleton-rails/version"
-require "skeleton-rails/engine"
-
-module Skeleton
-  require 'skeleton-rails/engine'
-end
--- gems/gems/skeleton-rails-1.2.0/lib/skeleton-rails/version.rb
+++
@@ -1,3 +0,0 @@
-module Skeleton
-  VERSION = "1.2.0"
-end
